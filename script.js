78e3f58302a1d2e179968f40432cf2c545fd0412
Noah Finer <noahfiner@gmail.com>
2017-09-16 18:47:53 -0600

    Make the site responsive

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+var toggleHamburger = function() {
+  $(".header-links, #hamburger-outer, .header-outer").toggleClass("expanded");
+}
+
 $(document).ready(function() {
   function checkScroll() {
+    $(".header-links, #hamburger-outer, .header-outer").removeClass("expanded");
+
     var scrollTop = $(window).scrollTop();
     if(scrollTop >= window.innerHeight*0.4) {
       $(".header-outer").removeClass("transparent");
     } else {
       $(".header-outer").addClass("transparent");
     }
+    if($(window).scrollLeft() > 0) {
+      window.scrollTo(0, scrollTop);
+    }
   }
+
+  $("#hamburger-outer").click(toggleHamburger);
 
   checkScroll();
   $(window).scroll(checkScroll);
